268fe011fd347dd9bf2833d85ec8c7af29cbd602
jzh-Bill <783026426@qq.com>
2022-02-27 21:35:32 -0600

    message changing

--- src/network/Message.java
+++ src/network/Message.java
@@ -1,18 +1,20 @@
-package network;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.net.InetAddress;
-import java.util.Vector;
-
-
-
 // Authors: Zhihao Jin and Donnie Beck
 // version: 1.0
 // Date modified: 02/27/2022
+
+package network;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.Vector;
+
 
 public class Message implements Serializable {
 	// The serialVersionUID is a identifier that is used to 
@@ -36,29 +38,135 @@ public class Message implements Serializable {
         this.IPlist = IPs; // The IP or IP lists the message may contain.
     }
     
-   // Since all information has to be serialized before they go to packet.
+    /**
+     * get of text
+     * @return the text of the message stored in the class
+     */
+    public String getText()
+    {
+    	return this.text;
+    }
+    
+    /**
+     * get of version
+     * @return the version of the message class
+     */
+    public double getVersion() {
+      return this.version;
+    }
+
+    /**
+     * get of IP list
+     * @return the IPlist in the message
+     */
+    public Vector<InetAddress> getIPList() {
+      return this.IPlist;
+    }
+
+    /**
+     * get of functioning mode of a message
+     * @return the mode of the message
+     */
+    public boolean getP2P() {
+      return this.isP2P;
+    }
+
+    /**
+     * get the usage of a this message
+     * @return the boolean value of Heartbeat of the message
+     */
+    public boolean getHeartbeating() {
+      return this.isHeartbeating;
+    }
+
+    /**
+     * set text message contained in a message
+     * @param textt the text desired to be stored in the text field
+     */
+    public void setText(String text) {
+      this.text = text;
+    }
+
+    /**
+     * set the meaning of the message
+     * @param meaning the meaning of the message
+     */
+    public void setMeaning(boolean meaning) {
+      this.isHeartbeating = meaning;
+    }
+    
+    
+    /**
+     * Serializes all data of message into bytes in order to be packed.
+	 * No parameter, use all data inside of message object.
+     * @return the bytes number of this object.
+     */
     private byte[] serializedMessage() {
-    	// Create a new message object to be serialized.
-        Message serialMessageObj = new Message(this.isP2P, this.isHeartbeating, this.text, this.IPlist);
+    		// Create a new message object to be serialized.
+            Message serialMessageObj = new Message(this.isP2P, this.isHeartbeating, this.text, this.IPlist);
         
-        // Serialize the message to be fitted into a buffer
-        ByteArrayOutputStream bufferstream = new ByteArrayOutputStream();
-        ObjectOutput messagebuffer;
-        
-        
-        // write the message objects in the buffer.
+            // Serialize the message to be fitted into a buffer
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ObjectOutput outBuffer;
         try {
-          messagebuffer = new ObjectOutputStream(bufferstream);
-          messagebuffer.writeObject(serialMessageObj);
-          messagebuffer.close();
-        } catch (IOException e1) {
-          e1.printStackTrace();
+            // write the message objects in the buffer
+        	outBuffer = new ObjectOutputStream(outStream);
+        	outBuffer.writeObject(serialMessageObj);
+        	outBuffer.close();
+        } catch (IOException io) {
+          io.printStackTrace();
           System.exit(-1);
         }
-        byte[] requestbuffer = bufferstream.toByteArray();
-
+        byte[] requestbuffer = outStream.toByteArray();
         return requestbuffer;
       }
     
+    /**
+     * Deserializes the incoming packet from the outside.
+     * @param incomingPacket the packet to extract data from
+     * @return to the taget message with the deserialized information.
+     */
+    public Message deserializer(DatagramPacket incomingPacket) {
+        ObjectInputStream inputStream;
+        Message responsemessage = null;
+    	try {
+            byte[] buffer = incomingPacket.getData();
+            inputStream = new ObjectInputStream(new ByteArrayInputStream(buffer));
+            responsemessage = (Message) inputStream.readObject();
+            inputStream.close();
+        } 
+    	catch (Exception e) 
+    	{
+    		e.printStackTrace();
+        }
+        return responsemessage;
+     }
     
+    
+    /**
+     * The function is used to packet the whole message with additional information.
+     * @param hostaddress the IP address of the receiver
+     * @param port the port number used to transmit the message
+     * @return the completed network package originated from the input message
+     */
+    public DatagramPacket createPacket(InetAddress hostIP, int port) {
+      // we first serialize with all the information we have in this object into bytes.
+      byte[] requestbuffer = serializedMessage();
+      // And we start to pack them with host IP address and port number
+      DatagramPacket outPacket = new DatagramPacket(requestbuffer, requestbuffer.length, hostIP, port);
+      return outPacket;
+    }
+    
+//    public static void main(String args[]) {
+//    	Vector<InetAddress> ips = new Vector<InetAddress>();
+//    	Message m1 = new Message(true, true, "hello", ips);   	
+//    	byte[] requestbuffer = m1.serializedMessage();
+//    	
+//    	InetAddress hostaddress = null;
+//    	DatagramPacket request = new DatagramPacket(requestbuffer, requestbuffer.length, hostaddress, 32);
+//    	
+//    	Message m2 = new Message(false, false, "", ips);
+//    	m2 = m2.deserializer(request);
+//    	System.out.println(m2.getText());
+//    }
 }
