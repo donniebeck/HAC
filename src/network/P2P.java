477700595d2de84f39fc912e85955df09ec56c99
donniebeck <donnie.beckii@gmail.com>
2022-02-27 20:59:29 -0600

    Update P2P.java

--- src/network/P2P.java
+++ src/network/P2P.java
@@ -1,20 +1,45 @@
 package network;
 
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class P2P
 {
-//	private static final int PORT_NO = 9876;
+	private static final int PORT_NO = 9876;
+	
+	private static String ipList[];
+	private static String myIP;
+	private static DatagramSocket socket;
 
 	public static void main(String[] args)
 	{
+		loadIPs();
+		createSocket();
+		
+		
+		
+	}
+
+	private static void createSocket()
+	{
+		try
+		{
+			socket = new DatagramSocket(PORT_NO);
+		} catch (SocketException e)
+		{
+			System.out.println("The datagram socket could not be created");
+			e.printStackTrace();
+		}
+	}
+
+	private static void loadIPs()
+	{
 		//Load myIP from txt file
 		ConfigReader configReader = new ConfigReader();
-		String myIP = configReader.getSingleIP("myIP.txt");
+		myIP = configReader.getSingleIP("myIP.txt");
 		
 		//Load IPs into array from txt file
-		String ipList[] = configReader.getIPListFromFile("ipList.txt");
-		
-		
+		ipList = configReader.getIPListFromFile("ipList.txt");
 	}
 
 }
